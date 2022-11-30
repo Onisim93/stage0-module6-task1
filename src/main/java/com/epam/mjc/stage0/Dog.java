bc035c7cc6664558491742f5b3ff684fe8b129f2
onisim_albarov <onisim93@gmail.com>
2022-11-30 17:28:24 +0400

    complete module6-task1

--- src/main/java/com/epam/mjc/stage0/Dog.java
+++ src/main/java/com/epam/mjc/stage0/Dog.java
@@ -1,5 +1,12 @@
 package com.epam.mjc.stage0;
 
-public class Dog {
+public class Dog extends Animal{
 
+    public static void main(String[] args) {
+        new Dog().getDescription();
+    }
+
+    public Dog() {
+        super("brown", 4, true);
+    }
 }
